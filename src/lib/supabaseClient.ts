85d1b3b0f37e7bfcb659e34302ebca62c754420a
Leonardo Rodrigues <leorodriguesoffice@gmail.com>
2024-11-19 15:44:14 -0300

    Ajuste nas variáveis

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Informações do Supabase
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const SUPABASE_URL = process.env.SUPABASE_URL!;
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 // Criando o cliente
