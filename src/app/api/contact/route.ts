69a2397778882a336d9fc54cc4170a11abcc98f9
Leonardo Rodrigues <leorodriguesoffice@gmail.com>
2025-01-23 12:14:21 -0300

    Refactor na rota da api usando App Router.

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,13 +1,9 @@
-// src/app/api/contact/route.ts
-
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-// No App Router, a função de rota é exportada como POST, GET, etc.:
 export async function POST(request: Request) {
   try {
     const { name, email, message } = await request.json();
-    console.log("Requisição recebida:", { name, email, message });
 
     const transporter = nodemailer.createTransport({
       service: "gmail",
@@ -17,17 +13,13 @@ export async function POST(request: Request) {
       },
     });
 
-    const mailOptions = {
+    await transporter.sendMail({
       from: process.env.EMAIL_USER,
       to: process.env.EMAIL_DESTINATION,
       subject: `Leorodrigues.dev - Contato de ${name}`,
       text: `Nome: ${name}\nEmail: ${email}\nMensagem: ${message}`,
-    };
+    });
 
-    const info = await transporter.sendMail(mailOptions);
-    console.log("Email enviado com sucesso:", info.response);
-
-    // Retorna um JSON com status 200
     return NextResponse.json({ message: "E-mail enviado com sucesso!" }, { status: 200 });
   } catch (error) {
     console.error("Erro ao enviar email:", error);
